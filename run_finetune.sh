8fa9c8275836df497b578e4b6471058a34e1f267
Lucas Rosenblatt <60412438+lurosenb@users.noreply.github.com>
2024-12-10 17:28:06 -0500

    Update run_finetune.sh

--- run_finetune.sh
+++ run_finetune.sh
@@ -28,6 +28,6 @@ for SUFFIX in "$@"; do
     --model "$MODEL" \
     --wandb-api-key "6243f6febde392b2ca8109041f9a9680a5d17993" \
     --validation-file "$VAL_FILE_ID" \
-    --n-epochs 5 \
-    --n-evals 5
+    --n-epochs 10 \
+    --n-evals 10
 done
