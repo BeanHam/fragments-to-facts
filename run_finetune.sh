c37754920fd5c7f2407c2dfa85c2ab0f5429d3d0
BeanHam <binhan96816@gmail.com>
2025-01-02 18:27:43 -0800

    reorganized code

--- run_finetune.sh
+++ run_finetune.sh
@@ -1,34 +1,37 @@
 #!/usr/bin/env bash
 
 # script to find corresponding train/val file ids from upload_map.json
-# example usage: ./run_fine_tune.sh 2
+# example usage: ./run_fine_tune.sh
 
 UPLOAD_MAP="upload_map.json"
-#MODEL="meta-llama/Meta-Llama-3.1-8B-Instruct-Reference"
-MODEL="mistralai/Mistral-7B-Instruct-v0.2"
+MODEL="meta-llama/Meta-Llama-3.1-8B-Instruct-Reference"
+SPLIT="train"
+WANDB_KEY="a73070a2ae35aa73562604c69dfc697278d19086"
 
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 <suffixes>"
-  echo "e.g. $0 2 3 4"
-  exit 1
-fi
-
-for SUFFIX in "$@"; do
-  # extract the file ids from the json map
-  TRAIN_FILE_ID=$(jq -r '."data_formatted_train_'${SUFFIX}'.jsonl"' "$UPLOAD_MAP")
-  VAL_FILE_ID=$(jq -r '."data_formatted_val_'${SUFFIX}'.jsonl"' "$UPLOAD_MAP")
-
-  if [ "$TRAIN_FILE_ID" = "null" ] || [ "$VAL_FILE_ID" = "null" ]; then
+if $MODEL=="meta-llama/Meta-Llama-3.1-8B-Instruct-Reference"; then
+    if $SPLIT=="train"; then
+        TRAIN_FILE_ID=$(jq -r '."llama_train.jsonl"' "$UPLOAD_MAP")
+        VAL_FILE_ID=$(jq -r '."llama_val.jsonl"' "$UPLOAD_MAP")
+    elif $SPLIT=="shadow"; then
+        TRAIN_FILE_ID=$(jq -r '."llama_shadow_train.jsonl"' "$UPLOAD_MAP")
+        VAL_FILE_ID=$(jq -r '."llama_shadow_val.jsonl"' "$UPLOAD_MAP")
+elif $MODEL=="mistralai/Mistral-7B-Instruct-v0.2"; then
+    if $SPLIT=="train"; then
+        TRAIN_FILE_ID=$(jq -r '."mistral_train.jsonl"' "$UPLOAD_MAP")
+        VAL_FILE_ID=$(jq -r '."mistral_val.jsonl"' "$UPLOAD_MAP")
+    elif $SPLIT=="shadow"; then
+        TRAIN_FILE_ID=$(jq -r '."mistral_shadow_train.jsonl"' "$UPLOAD_MAP")
+        VAL_FILE_ID=$(jq -r '."mistral_shadow_val.jsonl"' "$UPLOAD_MAP")
+    
+if [ "$TRAIN_FILE_ID" = "null" ] || [ "$VAL_FILE_ID" = "null" ]; then
     echo "no matching train/val entries found for suffix $SUFFIX in $UPLOAD_MAP"
-    continue
-  fi
+    done
 
-  # together fine-tuning command
-  together fine-tuning create \
-    --training-file "$TRAIN_FILE_ID" \
-    --model "$MODEL" \
-    --wandb-api-key "a73070a2ae35aa73562604c69dfc697278d19086" \
-    --validation-file "$VAL_FILE_ID" \
-    --n-epochs 10 \
-    --n-evals 10
-done
+# together fine-tuning command
+together fine-tuning create \
+  --model "$MODEL" \
+  --training-file "$TRAIN_FILE_ID" \  
+  --validation-file "$VAL_FILE_ID" \
+  --wandb-api-key "$WANDB_KEY" \  
+  --n-epochs 10 \
+  --n-evals 10
